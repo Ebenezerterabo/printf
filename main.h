22ba30d0aa79ab2290ee43fa96d6d2ec8ccb91e7
Ebenezer terabo <teraboebenezer@gmail.com>
2023-12-07 00:22:21 +0000

    Header file updated

--- main.h
+++ main.h
@@ -12,7 +12,7 @@ int _printf(const char *format, ...);
 int print_char(va_list ap);
 int print_str(va_list ap);
 int check_cases(va_list ap, const char *format);
-int _strlen(char *str);
+int _strlen(const char *str);
 int _putchr(char c);
 int put_int(int num);
 
