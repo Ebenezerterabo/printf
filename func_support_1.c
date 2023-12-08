5546c4e0ddfc08f407d18af6d9b6c0150f0f89f6
Ebenezer terabo <teraboebenezer@gmail.com>
2023-12-08 03:14:32 +0000

    Added rot13 func

--- func_support_1.c
+++ func_support_1.c
@@ -86,7 +86,7 @@ int print_binary(va_list ap)
 
 /**
  * string_rev - A function that reverses a string
- * @str: string
+ * @ap: string
  *
  * Return: number of characters printed
  */
@@ -103,4 +103,44 @@ int string_rev(va_list ap)
 		_putchr(str[i]);
 
 	return (len);
+}
+
+/**
+ * print_rot13 - A function that encodes a string using rot13
+ * @ap: argument pointer
+ *
+ * Return: a pointer to the string
+ */
+
+int print_rot13(va_list ap)
+{
+	char *str = va_arg(ap, char *);
+	int i, j;
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rev[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int print, count = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		print = 0;
+
+		for (j = 0; alpha[j] != '\0'; j++)
+		{
+			if (str[i] == alpha[j])
+			{
+				_putchr(rev[j]);
+				count++;
+				print = 1;
+				break;
+			}
+		}
+
+		if (!print)
+		{
+			_putchr(str[i]);
+			count++;
+		}
+	}
+
+	return (count);
 }
